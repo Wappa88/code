276bb8dc8cd3c702fe72d0adec620921b93201c7
Wappa88 <y.ryutaro.1988@outlook.jp>
2024-03-04 22:06:33 +0900

    add some scripts

---
+++ scripts/python/test01.py
@@ -0,0 +1,13 @@
+name = '''I will never hurt you, Believe me!!
+
+Even though, You constantly show me your ridiculous jokes every \"single\" day!! 
+I don't know how to deal with my emotion for you from now on!!
+'''
+reaction = "lmfao"
+reaction = "What \"aaaa\" heck..."
+
+lineBreak = "What are you\nDOING!!"
+
+print (name)
+print (reaction)
+print (lineBreak)
---
+++ scripts/python/test03.py
@@ -0,0 +1,4 @@
+num1, num2 = input("Add first number: "), input("Then, Second number: ")
+add = int(num1) + int(num2)
+
+print(add)
